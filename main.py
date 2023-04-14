29aef60701e41e0c43c2b4d51f01a9c7d267c011
Julia Nikolaeva <iamenates@gmail.com>
2023-04-14 10:21:16 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get('https://playground.learnqa.ru/api/hello')
+# payload = {"name": "User"}
+
+# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# print(response.text)
+
+# response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "User"})
+# parsed_responce_text = response.json()
+# print(parsed_responce_text["answer"])
+
+
+# Когда не уверены что придет JSON
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_responce_text = response.json()
+    print(parsed_responce_text)
+except JSONDecodeError:
+    print("Responce is not a JSON format")
